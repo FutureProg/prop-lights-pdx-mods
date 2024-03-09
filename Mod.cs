4093b88019d50b419a579049c4235084dc14498d
Nick Morrison <nickmorrison09@gmail.com>
2024-03-08 20:22:05 -0500

    published

--- Mod.cs
+++ Mod.cs
@@ -10,7 +10,7 @@ namespace Trejak.PropLightsPDXMods
     public class Mod : IMod
     {
         public static ILog log = LogManager.GetLogger($"{nameof(PropLightsPDXMods)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
-        private Setting m_Setting;
+        //private Setting m_Setting;
 
         public void OnLoad(UpdateSystem updateSystem)
         {
@@ -19,9 +19,9 @@ namespace Trejak.PropLightsPDXMods
             if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
                 log.Info($"Current mod asset at {asset.path}");
 
-            m_Setting = new Setting(this);
-            m_Setting.RegisterInOptionsUI();
-            GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+            //m_Setting = new Setting(this);
+            //m_Setting.RegisterInOptionsUI();
+            //GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
 
             //AssetDatabase.global.LoadSettings(nameof(PropLightsPDXMods), m_Setting, new Setting(this));
             updateSystem.UpdateAt<PropLightingSystem>(SystemUpdatePhase.GameSimulation);
@@ -30,11 +30,11 @@ namespace Trejak.PropLightsPDXMods
         public void OnDispose()
         {
             log.Info(nameof(OnDispose));
-            if (m_Setting != null)
-            {
-                m_Setting.UnregisterInOptionsUI();
-                m_Setting = null;
-            }
+            //if (m_Setting != null)
+            //{
+            //    m_Setting.UnregisterInOptionsUI();
+            //    m_Setting = null;
+            //}
         }
     }
 }
--- Setting.cs
+++ Setting.cs
@@ -65,7 +65,6 @@ namespace Trejak.PropLightsPDXMods
 
         public override void SetDefaults()
         {
-            throw new System.NotImplementedException();
         }
 
         public enum SomeEnum
@@ -83,46 +82,52 @@ namespace Trejak.PropLightsPDXMods
         {
             m_Setting = setting;
         }
+
         public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
         {
-            return new Dictionary<string, string>
-            {
-                { m_Setting.GetSettingsLocaleID(), "Mod settings sample" },
-                { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
-
-                { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Toggle" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Sliders" },
-                { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Button)), "Button" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Button)), $"Simple single button. It should be bool property with only setter or use [{nameof(SettingsUIButtonAttribute)}] to make button from bool property with setter and getter" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ButtonWithConfirmation)), "Button with confirmation" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.ButtonWithConfirmation)), $"Button can show confirmation message. Use [{nameof(SettingsUIConfirmationAttribute)}]" },
-                { m_Setting.GetOptionWarningLocaleID(nameof(Setting.ButtonWithConfirmation)), "is it confirmation text which you want to show here?" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Toggle" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)), $"Use bool property with setter and getter to get toggable option" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntSlider)), "Int slider" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntSlider)), $"Use int property with getter and setter and [{nameof(SettingsUISliderAttribute)}] to get int slider" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntDropdown)), "Int dropdown" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntDropdown)), $"Use int property with getter and setter and [{nameof(SettingsUIDropdownAttribute)}(typeof(SomeType), nameof(SomeMethod))] to get int dropdown: Method must be static or instance of your setting class with 0 parameters and returns {typeof(DropdownItem<int>).Name}" },
-
-                { m_Setting.GetOptionLabelLocaleID(nameof(Setting.EnumDropdown)), "Simple enum dropdown" },
-                { m_Setting.GetOptionDescLocaleID(nameof(Setting.EnumDropdown)), $"Use any enum property with getter and setter to get enum dropdown" },
-
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value1), "Value 1" },
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value2), "Value 2" },
-                { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value3), "Value 3" },
-            };
+            return new Dictionary<string, string>();
         }
 
         public void Unload()
         {
-
+            
         }
-    }
+
+
+        //public IEnumerable<KeyValuePair<string, string>> ReadEntries(IList<IDictionaryEntryError> errors, Dictionary<string, int> indexCounts)
+        //{
+        //    return new Dictionary<string, string>
+        //    {
+        //        { m_Setting.GetSettingsLocaleID(), "Mod settings sample" },
+        //        { m_Setting.GetOptionTabLocaleID(Setting.kSection), "Main" },
+
+        //        { m_Setting.GetOptionGroupLocaleID(Setting.kButtonGroup), "Buttons" },
+        //        { m_Setting.GetOptionGroupLocaleID(Setting.kToggleGroup), "Toggle" },
+        //        { m_Setting.GetOptionGroupLocaleID(Setting.kSliderGroup), "Sliders" },
+        //        { m_Setting.GetOptionGroupLocaleID(Setting.kDropdownGroup), "Dropdowns" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Button)), "Button" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.Button)), $"Simple single button. It should be bool property with only setter or use [{nameof(SettingsUIButtonAttribute)}] to make button from bool property with setter and getter" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.ButtonWithConfirmation)), "Button with confirmation" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.ButtonWithConfirmation)), $"Button can show confirmation message. Use [{nameof(SettingsUIConfirmationAttribute)}]" },
+        //        { m_Setting.GetOptionWarningLocaleID(nameof(Setting.ButtonWithConfirmation)), "is it confirmation text which you want to show here?" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.Toggle)), "Toggle" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.Toggle)), $"Use bool property with setter and getter to get toggable option" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntSlider)), "Int slider" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntSlider)), $"Use int property with getter and setter and [{nameof(SettingsUISliderAttribute)}] to get int slider" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.IntDropdown)), "Int dropdown" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.IntDropdown)), $"Use int property with getter and setter and [{nameof(SettingsUIDropdownAttribute)}(typeof(SomeType), nameof(SomeMethod))] to get int dropdown: Method must be static or instance of your setting class with 0 parameters and returns {typeof(DropdownItem<int>).Name}" },
+
+        //        { m_Setting.GetOptionLabelLocaleID(nameof(Setting.EnumDropdown)), "Simple enum dropdown" },
+        //        { m_Setting.GetOptionDescLocaleID(nameof(Setting.EnumDropdown)), $"Use any enum property with getter and setter to get enum dropdown" },
+
+        //        { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value1), "Value 1" },
+        //        { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value2), "Value 2" },
+        //        { m_Setting.GetEnumValueLocaleID(Setting.SomeEnum.Value3), "Value 3" },
+        //    };
+    }    
 }
